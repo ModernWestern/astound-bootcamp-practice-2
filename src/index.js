d159f6d06848e0a9edec369de1f057c2afd10a1f
Lobsang White <lobsangwhite@gmail.com>
2021-10-24 00:28:46 -0500

    Clean timeout

--- src/index.js
+++ src/index.js
@@ -1,34 +1,31 @@
-let productsInWishList = 0;
+var timeout;
+var productsInWishList = 0;
 
-let addToWishListEvent = new CustomEvent("ProductAddedToWishList");
-let removeFromWishListEvent = new CustomEvent("ProductRemovedFromWishList");
-let closeDialogEvent = new CustomEvent("CloseDialog");
+let addToWishListEvent = new CustomEvent('ProductAddedToWishList');
+let removeFromWishListEvent = new CustomEvent('ProductRemovedFromWishList');
+let closeDialogEvent = new CustomEvent('CloseDialog');
+let myEvent = new CustomEvent('myEvent');
 
-let messageDialog = document.querySelector(".dialog");
-let closeDialogButton = document.querySelector(".dialog__cancel-btn");
-let count = document.getElementById("wish-list-count");
-let wishListButtons = document.querySelectorAll(
-  "button.product-item__wish-list-btn"
-);
+let messageDialog = document.querySelector('.dialog');
+let closeDialogButton = document.querySelector('.dialog__cancel-btn');
+let count = document.getElementById('wish-list-count');
+let wishListButtons = document.querySelectorAll('button.product-item__wish-list-btn');
 
-messageDialog.addEventListener("CloseDialog", closeMessageDialog);
-closeDialogButton.addEventListener(
-  "ProductRemovedFromWishList",
-  removeFromWishList
-);
+messageDialog.addEventListener('CloseDialog', closeMessageDialog);
+closeDialogButton.addEventListener('ProductRemovedFromWishList', removeFromWishList);
 
-closeDialogButton.addEventListener("click", function () {
+closeDialogButton.addEventListener('click', function () {
   this.dispatchEvent(removeFromWishListEvent);
 });
 
-wishListButtons.forEach((button) =>
-  button.addEventListener("click", function () {
+wishListButtons.forEach(button =>
+  button.addEventListener('click', function () {
     this.dispatchEvent(addToWishListEvent);
   })
 );
 
-wishListButtons.forEach((button) =>
-  button.addEventListener("ProductAddedToWishList", addToWishList)
+wishListButtons.forEach(button =>
+  button.addEventListener('ProductAddedToWishList', addToWishList)
 );
 
 function addToWishList() {
@@ -36,12 +33,13 @@ function addToWishList() {
   count.innerText = productsInWishList;
   console.log(count);
 
-  messageDialog.style.display = "flex";
-  setTimeout(() => messageDialog.dispatchEvent(closeDialogEvent), 10000);
+  messageDialog.style.display = 'flex';
+  timeout = setTimeout(() => messageDialog.dispatchEvent(closeDialogEvent), 10000);
 }
 
 function closeMessageDialog() {
-  messageDialog.style.display = "none";
+  messageDialog.style.display = 'none';
+  clearTimeout(timeout);
 }
 
 function removeFromWishList() {
@@ -50,16 +48,12 @@ function removeFromWishList() {
   messageDialog.dispatchEvent(closeDialogEvent);
 }
 
-let closeTime = document.getElementById("hidden");
+let closeTime = document.getElementById('hidden');
 closeTime.onclick = closeDialog;
 
-function closeDialog() {
-  messageDialog.style.display = "none";
-}
-
-let close = document.getElementById("close");
+let close = document.getElementById('close');
 close.onclick = closeDialog;
 
 function closeDialog() {
-  messageDialog.style.display = "none";
+  messageDialog.style.display = 'none';
 }
